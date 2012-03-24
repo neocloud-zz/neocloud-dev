e861582e42ed9b08aa1c2cac75d85fec3bcc354a
NeoCloud <johnathon@neocloud.com>
2012-03-24 15:30:11 +0000

    neocloud 2nd commit

--- hello.js
+++ hello.js
@@ -6,10 +6,12 @@ http.createServer(function(request, response) {
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("NeoCloud Node.js Play Area - Hello World");  
 	response.end();
-}).listen(8888);
+}).listen(process.env.PORT);
+
+
 
 function execute(someFunction, value) {
 	someFunction(value);
 }
 
-execute(function(word){ console.log(word) }, "Hello");
+//execute(function(word){ console.log(word) }, "Hello");
