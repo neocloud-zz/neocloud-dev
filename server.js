bcedfbff86bc91344d7e3a56adf35b0c9e3f32c0
neocloud <johnathon@neocloud.com>
2012-03-24 15:50:00 +0000

    neocloud added server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+// Example: Launching one cluster working for each CPU
+
+var cluster = require('cluster');
+var http = require('http');
+var numCPUs = require('os').cpus().length; // Get the number of CPUs
+console.log('numCPUs: ' + numCPUs);
+
+if (cluster.isMaster) {
+  // Fork each worker onto its own thread
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  // When the worker dies, announce its PID and death
+  cluster.on('death', function(worker) {
+    console.log('worker ' + worker.pid + ' died');
+  });
+}
+else {
+  // If the worker is not the master process, run it as an HTTP server
+  http.Server(function(req, res) {
+    res.writeHead(200);
+    res.end("hello world\n");
+  }).listen(process.env.PORT, "0.0.0.0");
+}
+    // We're using the special Cloud9 IDE port and hostname here;
+    // you'll probably just want something like (8080, "127.0.0.1")
