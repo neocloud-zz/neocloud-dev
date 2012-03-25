a31f42690b069737739540940b7b5af89a89865a
NeoCloud <johnathon@neocloud.com>
2012-03-25 15:27:26 +0000

    updated for mongo

--- server.js
+++ server.js
@@ -35,3 +35,29 @@ else {
 
 process.title ="gensvr001.ec2.neocloud.com";
 console.log("Server %s listening at http://localhost" + (PORT === 80 ? "" : ":" + PORT) + "/", process.title);
+
+// app.js
+//var databaseUrl = "mydb"; // "username:password@example.com/mydb"
+var databaseUrl = "mongodb://johnathon:johnn0@staff.mongohq.com:10024/neonode";
+var collections = ["users", "reports"];
+var db = require("mongojs").connect(databaseUrl, collections);
+
+// app.js
+db.users.save({email: "srirangan@gmail.com", password: "iLoveMongo", sex: "male"}, function(err, saved) {
+  if( err || !saved ) console.log("User not saved");
+  else console.log("User saved");
+});
+
+// app.js
+db.users.save({email: "johnathon@gmail.com", password: "iLoveMongo", sex: "female"}, function(err, saved) {
+  if( err || !saved ) console.log("User not saved");
+  else console.log("User saved");
+});
+
+// app.js
+db.users.find({sex: "female"}, function(err, users) {
+  if( err || !users) console.log("No female users found");
+  else users.forEach( function(femaleUser) {
+    console.log(femaleUser);
+  } );
+});
